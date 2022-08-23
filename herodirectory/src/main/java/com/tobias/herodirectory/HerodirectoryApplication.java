0d985a797f06a0688b92386d34802d5016a28ef7
Tobias Carothers <wogrim.pseul@gmail.com>
2022-08-23 14:01:41 -0700

    create STS project

---
+++ herodirectory/src/main/java/com/tobias/herodirectory/HerodirectoryApplication.java
@@ -0,0 +1,13 @@
+package com.tobias.herodirectory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HerodirectoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HerodirectoryApplication.class, args);
+	}
+
+}
---
+++ herodirectory/src/test/java/com/tobias/herodirectory/HerodirectoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tobias.herodirectory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HerodirectoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
